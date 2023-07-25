27479012094baa16b19deb896df2845c9fed353a
meet <meet.devstree@gmail.com>
2023-07-25 11:35:47 +0530

    - updated gradle

--- app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvmapp
+package com.example.mvvmapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
