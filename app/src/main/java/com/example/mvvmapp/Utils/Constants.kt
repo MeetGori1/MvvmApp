a8664b42ee156e0283b3ab03093508eadf9b2324
meet <meet.devstree@gmail.com>
2023-07-26 18:06:43 +0530

    - Added pagination

--- app/src/main/java/com/example/mvvmapp/Utils/Constants.kt
+++ app/src/main/java/com/example/mvvmapp/Utils/Constants.kt
@@ -8,9 +8,9 @@ id name : meet_g
 */
 
 object Constants{
-//    const val CLIENT_ID = "y5YedCBH7D2gZbmmWOZQy7FJCDsSbZH1ZchJnIzBCpA" //demo
+    const val CLIENT_ID = "y5YedCBH7D2gZbmmWOZQy7FJCDsSbZH1ZchJnIzBCpA" //demo
 //    const val CLIENT_ID = "gWNPAi0h5cT8Yi_ggxm2UjNOqF-FrUsihLM-JDdgB1g" //demo 2
-    const val CLIENT_ID = "yNv_q2pic9usJ7SZ3LAJQvsaTqw7AqvGeu3WC5-ZnLE" //demo 3
+//    const val CLIENT_ID = "yNv_q2pic9usJ7SZ3LAJQvsaTqw7AqvGeu3WC5-ZnLE" //demo 3
     const val END_POINT = "/photos"
     const val END_POINT_SEARCH = "/search/photos"
     const val BASE_URL = "https://api.unsplash.com"
--- app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmapp/view/MainActivity.kt
@@ -1,53 +1,79 @@
 package com.example.mvvmapp.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+import com.example.mvvmapp.adapter.DiffUtilListAdapter
 import com.example.mvvmapp.api.Results
 import com.example.mvvmapp.databinding.ActivityMainBinding
 import com.example.mvvmapp.model.ImageItem
-import com.example.mvvmapp.adapter.DiffUtilListAdapter
 import com.example.mvvmapp.viewmodel.MainViewModel
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var mainViewModel: MainViewModel
     lateinit var adapter: DiffUtilListAdapter
+    lateinit var layoutManager: GridLayoutManager
     var list: ArrayList<ImageItem>? = null
+
+    var page = 1
+    var perPage = 10
+    var isLoading: Boolean = false
+    var isLastPage: Boolean = false
+    var isScrolling: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        layoutManager = GridLayoutManager(this,4)
+        binding.rwRecyclerView.layoutManager = layoutManager
 
+//        val snapHelper: SnapHelper = PagerSnapHelper()
+//        snapHelper.attachToRecyclerView(binding.rwRecyclerView)
         adapter = DiffUtilListAdapter()
         binding.rwRecyclerView.adapter = adapter
         list = ArrayList()
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        apiCall()
+        apiCall(1)
+        scrollListner()
     }
 
-    private fun apiCall() {
-        mainViewModel.getQuotes(1, 10).observe(this) {
+    private fun apiCall(page:Int) {
+        mainViewModel.getQuotes(page,perPage).observe(this) {
             when (it) {
-
                 is Results.Error -> {
-                    Toast.makeText(this, it.errorMessage?.errors?.get(0)?.toString() ?: "water", Toast.LENGTH_LONG)
+                    Toast.makeText(
+                        this,
+                        it.errorMessage?.errors?.get(0)?.toString() ?: "water",
+                        Toast.LENGTH_LONG
+                    )
                         .show()
                 }
 
                 is Results.Success -> {
                     if (it.data!!.isNotEmpty()) {
-                        Toast.makeText(
-                            this,
-                            it.data!![0].description,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        for (i in it.data){
+//                        Toast.makeText(
+//                            this,
+//                            it.data!![0].description,
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                        list?.clear()
+                        for (i in it.data) {
                             list?.add(i)
                         }
                         adapter.submitList(list)
-                    }else{
+                        adapter.notifyItemInserted(adapter.itemCount+1)
+                    } else {
                         Toast.makeText(this, "No Data Found", Toast.LENGTH_SHORT).show()
                     }
 
@@ -57,6 +83,32 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    fun scrollListner(){
+        binding.rwRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                var visibleItemCount = layoutManager.childCount
+                val totalItemCount: Int = layoutManager.itemCount
+                val firstVisibleItemPosition: Int = layoutManager.findFirstVisibleItemPosition()
+                if (!isLoading && !isLastPage) {
+                    if (visibleItemCount + firstVisibleItemPosition >= totalItemCount
+                        && firstVisibleItemPosition >= 0
+                    ) {
+//                        binding.progressbar.visibility = View.VISIBLE
+                        page++
+                        apiCall(page)
+                    }
+                }
+            }
+        })
     }
 
 }
